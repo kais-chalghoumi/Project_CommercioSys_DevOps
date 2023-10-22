323121373a660e92199e434d74f59910a88586a8
kais-chalghoumi <kais.chalghoumi@esprit.tn>
2023-10-22 20:40:38 +0100

    correction

---
+++ src/test/java/tn/esprit/devops_project/DevOpsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.devops_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
