9ff56529aa189d0f80306e42dc068ff85b806d1c
kais-chalghoumi <92551101+kais-chalghoumi@users.noreply.github.com>
2023-10-31 13:49:05 +0100

    Revert "Activity sector"

--- src/main/java/tn/esprit/devops_project/services/ActivitySectorImpl.java
+++ src/main/java/tn/esprit/devops_project/services/ActivitySectorImpl.java
@@ -9,7 +9,6 @@ import tn.esprit.devops_project.services.Iservices.IActivitySector;
 import java.util.List;
 
 @Service
-
 @AllArgsConstructor
 public class ActivitySectorImpl implements IActivitySector {
 
