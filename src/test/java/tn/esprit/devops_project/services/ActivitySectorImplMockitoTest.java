050e9a5439a43dd30e6cc1202fcdfa1d9a60fb79
chourouk-zioud <chourouk.zioud@esprit.tn>
2023-11-01 23:55:58 +0100

    test

--- src/test/java/tn/esprit/devops_project/services/ActivitySectorImplMockitoTest.java
+++ src/test/java/tn/esprit/devops_project/services/ActivitySectorImplMockitoTest.java
@@ -1,17 +1,50 @@
 package tn.esprit.devops_project.services;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tn.esprit.devops_project.entities.ActivitySector;
+import tn.esprit.devops_project.repositories.ActivitySectorRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class ActivitySectorImplMockitoTest {
+
+    @Mock
+    ActivitySectorRepository activitySectorRepository;
+
+    @InjectMocks
+    ActivitySectorImpl activitySectorService;
+
+    ActivitySector activitySector = new ActivitySector(1L,"touta","batta",null);
+
+    List<ActivitySector> activitySectorList = new ArrayList<ActivitySector>(){
+        {
+            add(new ActivitySector(2L,"touta2","batta2",null));
+            add(new ActivitySector(3L,"touta3","batta3",null));
+        }
+    };
 
     @Test
     void retrieveAllActivitySectors() {
+        Mockito.when(activitySectorRepository.findAll()).thenReturn(activitySectorList);
+        List<ActivitySector> actlist = activitySectorService.retrieveAllActivitySectors();
+        Assertions.assertEquals(2,actlist.size());
     }
 
     @Test
     void addActivitySector() {
+        Mockito.when(activitySectorRepository.save(Mockito.any(ActivitySector.class))).thenReturn(activitySector);
+        ActivitySector sectoract = activitySectorService.addActivitySector(activitySector);
+        Assertions.assertEquals(1L,sectoract.getIdSecteurActivite());
     }
 
     @Test
