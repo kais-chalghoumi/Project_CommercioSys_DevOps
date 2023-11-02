7051ffef9ae8f7be0d2493bd685820dc36126202
wathek28 <wathek.mathlouthi1@esprit.tn>
2023-11-02 14:03:29 +0100

    wathekVillage

---
+++ src/test/java/tn/esprit/devops_project/repositories/ProductRepositoryTest.java
@@ -0,0 +1,16 @@
+package tn.esprit.devops_project.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductRepositoryTest {
+
+    @Test
+    void findByCategory() {
+    }
+
+    @Test
+    void findByStockIdStock() {
+    }
+}
