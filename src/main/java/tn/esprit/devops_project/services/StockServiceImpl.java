5c7780d07bad32ccc20752aaa92e6f5d839c18dd
Mohamed-Yermani <mohamed.yermani@esprit.tn>
2023-11-02 12:39:05 +0100

    upp

--- src/main/java/tn/esprit/devops_project/services/StockServiceImpl.java
+++ src/main/java/tn/esprit/devops_project/services/StockServiceImpl.java
@@ -3,10 +3,7 @@ package tn.esprit.devops_project.services;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import tn.esprit.devops_project.services.Iservices.IStockService;
-import tn.esprit.devops_project.entities.Stock;
-import tn.esprit.devops_project.repositories.StockRepository;
 
-import java.util.List;
 
 @Service
 @AllArgsConstructor
